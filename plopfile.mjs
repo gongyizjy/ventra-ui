588b355888d1ba5ea2fafb7858ae93bc6b71da60
gy <1340837581@qq.com>
2025-05-06 14:14:22 +0800

    配置组件模板

--- plopfile.mjs
+++ plopfile.mjs
@@ -1,20 +1,45 @@
 export default function (plop) {
-  plop.setGenerator('component', {
-    description: 'Create a new React component',
+  plop.setGenerator("component", {
+    description: "Create a new React component",
     prompts: [
       {
-        type: 'input',
-        name: 'name',
-        message: 'Component name (PascalCase):'
-      }
+        type: "input",
+        name: "name",
+        message: "Component name (PascalCase):",
+      },
     ],
     actions: [
       {
-        type: 'addMany',
-        destination: 'packages/react/{{kebabCase name}}',
-        base: '.plop-templates/component',
-        templateFiles: '.plop-templates/component/**'
-      }
-    ]
+        type: "add",
+        path: "packages/react/{{kebabCase name}}/package.json",
+        templateFile: "template/component/package.json.hbs",
+      },
+      {
+        type: "add",
+        path: "packages/react/{{kebabCase name}}/eslint.config.mjs",
+        templateFile: "template/component/eslint.config.mjs.hbs",
+      },
+      // eslint
+      {
+        type: "add",
+        path: "packages/react/{{kebabCase name}}/tsconfig.json",
+        templateFile: "template/component/tsconfig.json.hbs",
+      },
+      {
+        type: "add",
+        path: "packages/react/{{kebabCase name}}/src/index.ts",
+        templateFile: "template/component/src/index.ts.hbs",
+      },
+      {
+        type: "add",
+        path: "packages/react/{{kebabCase name}}/src/{{kebabCase name}}.tsx",
+        templateFile: "template/component/src/component.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "packages/react/{{kebabCase name}}/src/{{kebabCase name}}.scss",
+        templateFile: "template/component/src/component.scss.hbs",
+      },
+    ],
   });
 }
---
+++ template/component/eslint.config.mjs.hbs
@@ -0,0 +1,3 @@
+import { config } from "@repo/eslint-config/react-package";
+/** @type {import("eslint").Linter.Config} */
+export default config;
