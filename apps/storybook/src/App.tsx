a099a71fec9a5291f653861307d7c7f955243e6e
gy <1340837581@qq.com>
2025-05-08 15:47:55 +0800

    完成部分button组件内容

--- apps/storybook/src/App.tsx
+++ apps/storybook/src/App.tsx
@@ -1,37 +1,28 @@
-import { useState } from 'react'
-import { Button } from 'ventra-ui'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+import { Button } from "ventra-ui";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <Button>Button</Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button type="primary" size="large">
+        测试
+      </Button>
+      <Button type="default" disabled>
+        测试
+      </Button>
+      <Button type="dashed" size="small">
+        测试
+      </Button>
+      <Button type="text" size="large">
+        测试
+      </Button>
+      <Button type="link" size="large">
+        测试
+      </Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- packages/react/button/src/button.tsx
+++ packages/react/button/src/button.tsx
@@ -1,11 +1,77 @@
+import React, { MouseEvent } from "react";
+import classNames from "classnames";
 import "./button.scss";
+
+type PresetColors =
+  | "blue"
+  | "purple"
+  | "cyan"
+  | "green"
+  | "magenta"
+  | "pink"
+  | "red"
+  | "orange"
+  | "yellow"
+  | "volcano"
+  | "geekblue"
+  | "lime"
+  | "gold";
 interface ButtonProps {
+  type?: "primary" | "default" | "dashed" | "text" | "link";
+  size?: "small" | "medium" | "large";
+  shape?: "default" | "circle" | "round";
+  htmlType?: "button" | "submit" | "reset";
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  loading?: boolean | { delay?: number; icon?: React.ReactNode };
+  color?: "default" | "primary" | "danger" | PresetColors;
+  autoInsertSpace?: boolean;
+  disabled?: boolean;
+  icon?: React.ReactNode;
+  iconPosition?: "start" | "end";
+  herf?: string;
   children?: React.ReactNode;
 }
-export default function Button({ children }: ButtonProps) {
+export default function Button({
+  children,
+  type = "default",
+  size = "medium",
+  htmlType = "button",
+  onClick,
+  loading = false,
+  shape = "default",
+  color = "default",
+  autoInsertSpace = true,
+  disabled = false,
+  icon = null,
+  iconPosition = "start",
+  herf,
+}: ButtonProps) {
+
   return (
-    <button className="btn">
-      <span>测试{children}</span>
+    <button
+      className={classNames("ventra-btn", {
+        [`ventra-btn-${type}`]: type,
+        "ventra-btn-variant-solid": type === "primary",
+        "ventra-btn-variant-outlined": type === "default",
+        "ventra-btn-variant-text": type === "text",
+        "ventra-btn-variant-link": type === "link",
+        "ventra-btn-variant-dashed": type === "dashed",
+        "ventra-btn-color-default": type !== "primary" || "link",
+        "ventra-btn-color-primary": type === "primary",
+        "ventra-btn-color-link": type === "link",
+        [`ventra-btn-lg`]: size === "large",
+        [`ventra-btn-sm`]: size === "small",
+      })}
+      type={htmlType}
+      disabled={disabled}
+    >
+      {icon && iconPosition === "start" && (
+        <span className="ventra-btn-icon">{icon}</span>
+      )}
+      <span style={{ color: color }}>{children}</span>
+      {icon && iconPosition === "end" && (
+        <span className="ventra-btn-icon">{icon}</span>
+      )}
     </button>
   );
 }
